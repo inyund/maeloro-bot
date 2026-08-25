0b1e4c7410e1cf040ba598bd48491757e93defdc
inyund <noreply@inyund>
2026-08-25 15:11:37 +0700

    v55 Secure wsProxy: AC4 224B (webAuthToken, 160B server entry), versions 45->55

--- openkore/src/Network/Receive/kRO/RagexeRE_Maelo.pm
+++ openkore/src/Network/Receive/kRO/RagexeRE_Maelo.pm
@@ -1,4 +1,6 @@
 # MaeloRO: kRO_RagexeRE_0 + 0AC4 account_server_info (server replies with it to 0064 login)
+# 2026-08-25 "Secure wsProxy" update: version 45->55, AC4 grew to 224B:
+#   pid len | AID(4) auth(4) li2(4) | sex@16 | x30 | webAuthToken Z17 @47 | server entries (160B) @64
 package Network::Receive::kRO::RagexeRE_Maelo;
 use strict;
 use base qw(Network::Receive::kRO::RagexeRE_0);
@@ -7,7 +9,7 @@ sub new {
 	my ($class) = @_;
 	my $self = $class->SUPER::new(@_);
 	my %packets = (
-		'0AC4' => ['account_server_info', 'v a4 a4 a4 a4 a26 C x17 a*', [qw(len sessionID accountID sessionID2 lastLoginIP lastLoginTime accountSex serverInfo)]],
+		'0AC4' => ['account_server_info', 'v a4 a4 a4 C x30 Z17 a*', [qw(len sessionID accountID sessionID2 accountSex webAuthToken serverInfo)]],
 		'09A0' => ['sync_received_characters', 'V', [qw(sync_Count)]],
 		'020D' => ['sync_received_characters', 'v', []], # MaeloRO uses 0x020D (len 4) as charlist notify
 	);
--- openkore/src/Network/Send/kRO/RagexeRE_Maelo.pm
+++ openkore/src/Network/Send/kRO/RagexeRE_Maelo.pm
@@ -1,4 +1,4 @@
-# MaeloRO send: same as RagexeRE_0 (master_login 0064 verified live)
+# MaeloRO send: same as RagexeRE_0 (master_login 0064 verified live at version 55)
 package Network::Send::kRO::RagexeRE_Maelo;
 use strict;
 use base qw(Network::Send::kRO::RagexeRE_0);
@@ -7,7 +7,7 @@ sub new {
 	my ($class) = @_;
 	my $self = $class->SUPER::new(@_);
 	my %packets = (
-		'09A1' => ['sync_received_characters'], # reply to 09A0 charlist notify
+		'09A1' => ['sync_received_characters'], # reply to 0x020D charlist notify
 	);
 	$self->{packet_list}{$_} = $packets{$_} for keys %packets;
 	return $self;
